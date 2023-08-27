4229d632f290c58c6f3a3b841c668eb974a87396
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-27 19:49:40 +0300

    passed all the tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",12,"M","brown","neuroticism", 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Rowdy",10,"F","brown","neuroticism", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Tabitha",12,"F","black","cooperative", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",12,"M","black","cooperative", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Melissa",9,"F","white","prone to anxiety", 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Grinch",8,"M","black","outgoing", 0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Wendy",12,"F","yellow","adventurous", 1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES(NULL,12,"F","black","cooperative", 0);
